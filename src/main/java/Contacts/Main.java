de85b13e851baa5d5fce0da43525cc4c6b0bee1b
Safi <safiullahkakar2020@outlook.com>
2024-11-27 00:31:53 +0430

    Added stage 1 solution code

--- src/main/java/Contacts/Main.java
+++ src/main/java/Contacts/Main.java
@@ -1,4 +1,4 @@
-package Contacts-Java;
+package Contacts;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/Contacts/Singleton.java
@@ -0,0 +1,24 @@
+package Contacts;
+
+import java.util.Scanner;
+
+class SingleContact {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the name of the person:");
+        String firstName = sc.nextLine();
+
+        System.out.println("Enter the surname of the person:");
+        String lastName = sc.nextLine();
+        System.out.println("Enter the number:");
+        String number = sc.nextLine();
+
+        System.out.println();
+        System.out.println("A record created!");
+        System.out.println("A Phone Book with a single record created!");
+
+        sc.close();
+
+    }
+}
